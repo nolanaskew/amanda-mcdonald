25f1270204772f44cdca4711b57a89000393ccde
Nolan Askew <nolanaskew@gmail.com>
2021-08-10 17:03:45 -0400

    Added the image gallery

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,46 @@
 import HelloWorld from '../component/hello-world'
-
+import React, { useState, useCallback } from "react";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from "./photos";
+  
 export default function Home() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   return (
+    <div>
+      <title>Amanda McDonald</title>
     <div className="example">
       <HelloWorld />
+    </div>
+    <div>
+      <Gallery photos={photos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
     </div>
   )
 }
