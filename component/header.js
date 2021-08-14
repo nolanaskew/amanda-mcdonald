7b0c50ba7d9a9df6552c3ccf5b36eca605fc1cf6
Nolan Askew <nolanaskew@gmail.com>
2021-08-14 19:52:27 -0400

    Added dropdown to header

--- component/header.js
+++ component/header.js
@@ -5,10 +5,17 @@ export default function Header() {
     <div className={css.header}>
       <a href="#home" className={css.logo}>Amanda McDonald</a>
       <div className={css.headerRight}>
-        <a className={css.active} href="#art">Art</a>
-        <a href="#about">About</a>
-        <a href="#contact">Contact</a>
-      </div>
+        <div className={css.dropdown}>
+          <button className={css.dropbtn}>Art</button>
+          <div className={css.dropdownContent}>
+            <a href="/drawing">Drawing</a>
+            <a href="/sculpture">Sculpture</a>
+            <a href="/painting">Painting</a>
+          </div>
+        </div>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </div> 
     </div>
   )
 }
